b1905658248e638a37be823aed05a90f5d0ec60f
Dawson Mortenson <dawson.mortenson@gmail.com>
2018-05-18 17:25:08 -0700

    create a ring module that spawns linkable processes

--- lib/ring.ex
+++ lib/ring.ex
@@ -1,18 +1,33 @@
 defmodule Ring do
-  @moduledoc """
-  Documentation for Ring.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Ring.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def create_p(n) do
+    1..n |> Enum.map(fn _ -> spawn(fn -> loop() end) end)
   end
+
+  def loop do
+    receive do
+      {:link, link_to} when is_pid(link_to) ->
+        Process.link(link_to)
+        loop()
+
+      :crash ->
+        1/0
+    end
+  end
+
+  def link_p(ps) do
+    link_p(ps, [])
+  end
+
+  def link_p([p1, p2|rest], linked_ps) do
+    send(p1, {:link, p2})
+    link_p([p2|rest], [p1|linked_ps])
+  end
+
+  def link_p([p|[]], linked_ps) do
+    first_p = linked_ps |> List.last()
+    send(p, {:link, first_p})
+    :ok
+  end
+
 end
